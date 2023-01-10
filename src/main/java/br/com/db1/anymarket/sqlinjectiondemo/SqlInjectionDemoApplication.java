fcad313590a2420f98409b9894bf4357680ed610
Jhonnatan Vieira <jfcvieira.1234@gmail.com>
2023-01-10 14:46:31 -0300

    Criação do projeto

---
+++ src/main/java/br/com/db1/anymarket/sqlinjectiondemo/SqlInjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.db1.anymarket.sqlinjectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlInjectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlInjectionDemoApplication.class, args);
+	}
+
+}
